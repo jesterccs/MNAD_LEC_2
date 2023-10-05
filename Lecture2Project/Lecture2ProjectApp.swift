aeac91c0ebb292406c54dc003af1374b4488498e
Chamika <chamikachathushka8@gmail.com>
2023-10-05 16:46:59 +0530

    Initial Commit

---
+++ Lecture2Project/Lecture2ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lecture2ProjectApp.swift
+//  Lecture2Project
+//
+//  Created by Chamika Sakalasuriya on 2023-10-05.
+//
+
+import SwiftUI
+
+@main
+struct Lecture2ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
